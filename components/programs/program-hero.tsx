db24ff01d0c75f718ba34168ab56f519178c55a8
Vision Matrix Institute <akhtar99n@gmail.com>
2026-07-23 23:04:01 +0530

    Wire up the program brochure download on the Data Center Specialist page
    The hero's "Download Brochure" button previously had no link. It now downloads the
    real program brochure PDF.

--- components/programs/program-hero.tsx
+++ components/programs/program-hero.tsx
@@ -52,9 +52,11 @@ export function ProgramHero() {
               </Button>
             </MagneticButton>
             <MagneticButton>
-              <Button size="lg" variant="outline" className="border-white/30 text-white hover:bg-white/10 hover:text-white bg-transparent">
-                <Download className="mr-2 h-5 w-5" />
-                Download Brochure
+              <Button asChild size="lg" variant="outline" className="border-white/30 text-white hover:bg-white/10 hover:text-white bg-transparent">
+                <a href="/brochures/electrical-design-data-center-brochure.pdf" download>
+                  <Download className="mr-2 h-5 w-5" />
+                  Download Brochure
+                </a>
               </Button>
             </MagneticButton>
           </motion.div>
